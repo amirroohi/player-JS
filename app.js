96b37e415194eda315de793e1f71407a1e0c41d2
amirroohi <amirroohi@yahoo.com>
2021-09-07 23:39:20 +0430

    edition two

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const pausePlay = document.querySelector(".music-pause");
 const forward = document.querySelector(".music-forward");
 const backward = document.querySelector(".music-backward");
+const timeline = document.querySelector(".time-line");
+
